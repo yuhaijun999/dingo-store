cdf46578de4d013fe0b6e312861dd43d1cea7598
Haijun Yu <yuhaijun999@hotmail.com>
2025-05-27 11:10:42 +0800

    [fix][index] Modify the priority of the task ivf_pq train to get it executed quickly.

--- src/vector/vector_index_ivf_pq.cc
+++ src/vector/vector_index_ivf_pq.cc
@@ -96,8 +96,10 @@ butil::Status VectorIndexIvfPq::AddOrUpsertWrapper(const std::vector<pb::common:
       if (BAIDU_UNLIKELY(is_first_add_or_upsert_)) {
         is_first_add_or_upsert_ = false;
         DINGO_LOG(INFO) << fmt::format(
-            "[vector_index.ivf_pq][id({})] now is {}({}) .  first add or upsert. this : {:#x}", Id(),
-            static_cast<int>(inner_index_type_), GetInnerIndexTypeName(), reinterpret_cast<int64_t>(this));
+            "[vector_index.ivf_pq][id({})] now is {}({}) .  first add or upsert. this : {:#x} "
+            "last_add_or_upsert_second_ : {}({})",
+            Id(), static_cast<int>(inner_index_type_), GetInnerIndexTypeName(), reinterpret_cast<int64_t>(this),
+            last_add_or_upsert_second_, Helper::FormatTime(last_add_or_upsert_second_, "%Y-%m-%d %H:%M:%S"));
       }
       return status;
     } else if (!status.ok() && pb::error::Errno::EVECTOR_NOT_TRAIN != status.error_code()) {
@@ -123,9 +125,11 @@ butil::Status VectorIndexIvfPq::AddOrUpsertWrapper(const std::vector<pb::common:
     last_add_or_upsert_second_ = Helper::Timestamp();
     if (BAIDU_UNLIKELY(is_first_add_or_upsert_)) {
       is_first_add_or_upsert_ = false;
-      DINGO_LOG(INFO) << fmt::format("[vector_index.ivf_pq][id({})] now is {}({}) .  first add or upsert. this : {:#x}",
-                                     Id(), static_cast<int>(inner_index_type_), GetInnerIndexTypeName(),
-                                     reinterpret_cast<int64_t>(this));
+      DINGO_LOG(INFO) << fmt::format(
+          "[vector_index.ivf_pq][id({})] now is {}({}) .  first add or upsert. this : {:#x} "
+          "last_add_or_upsert_second_ : {}({})",
+          Id(), static_cast<int>(inner_index_type_), GetInnerIndexTypeName(), reinterpret_cast<int64_t>(this),
+          last_add_or_upsert_second_, Helper::FormatTime(last_add_or_upsert_second_, "%Y-%m-%d %H:%M:%S"));
     }
   }
 
@@ -187,9 +191,11 @@ butil::Status VectorIndexIvfPq::Search(const std::vector<pb::common::VectorWithI
 
   if (BAIDU_UNLIKELY(is_first_search_)) {
     is_first_search_ = false;
-    DINGO_LOG(INFO) << fmt::format("[vector_index.ivf_pq][id({})] now is {}({}) .  first search.  this : {:#x}", Id(),
-                                   static_cast<int>(inner_index_type_), GetInnerIndexTypeName(),
-                                   reinterpret_cast<int64_t>(this));
+    DINGO_LOG(INFO) << fmt::format(
+        "[vector_index.ivf_pq][id({})] now is {}({}) .  first search.  this : {:#x} last_add_or_upsert_second_ : "
+        "{}({})",
+        Id(), static_cast<int>(inner_index_type_), GetInnerIndexTypeName(), reinterpret_cast<int64_t>(this),
+        last_add_or_upsert_second_, Helper::FormatTime(last_add_or_upsert_second_, "%Y-%m-%d %H:%M:%S"));
   }
 
   return butil::Status::OK();
